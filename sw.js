58a7104abf40203a909e31b2837fb1ba57750eaa
maria carrero <mariacarrerochacon267@gmail.com>
2022-02-14 16:53:14 -0400

    add all...

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js');
 
 const STATIC_CACHE='static-v1';
-const DYNAMIC_CACHE='dynamic-v1';
+const DYNAMIC_CACHE='dynamic-v2';
 const INMUTABLE_CACHE='inmutable-v1';
 
 
@@ -43,16 +43,27 @@ self.addEventListener('install',e=>{
 })
 
 
-self.addEventListener('activate', e=>{
-    const respuesta= caches.keys().then(keys=>{
-        keys.forEach(key=>{
-            if(key !== STATIC_CACHE && key.includes('static')){
-                return caches.delete(key)
+self.addEventListener('activate', e => {
+ 
+    const respuesta = caches.keys().then( keys => {
+ 
+        keys.forEach( key => {
+ 
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
             }
-        })
-    })
-    e.waitUntil( respuesta )
-})
+ 
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+ 
+        });
+ 
+    });
+ 
+    e.waitUntil( respuesta );
+ 
+});
 
 //ahora se va a implementar la estrategia del cache:
 //Cache con Network fallback
